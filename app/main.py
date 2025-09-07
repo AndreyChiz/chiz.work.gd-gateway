90dab1c3af45636906b8d886cb972b0f70d97d29
AndreyChiz <andrey.chizhov.dev@gmail.com>
2025-09-07 20:50:15 +0400

    ✨ feat(auth): add mock get /me endpoint

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from fastapi import Cookie, FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 from .middlewares.debug_log_middleware import debug_log_middleware
+from fastapi.responses import JSONResponse
 
 
 app = FastAPI(
@@ -34,13 +35,22 @@ app.add_middleware(
 app.middleware("http")(debug_log_middleware)
 
 
-mock_user = {
-    "id": 1,
-    "username": "achi",
-    "email": "achi@example.com",
-    "role": "admin",
-    "permissions": ["read", "write", "delete"],
-}
+mock_users = [
+    {
+        "id": 1,
+        "username": "achi",
+        "email": "achi@example.com",
+        "role": "admin",
+        "permissions": ["read", "write", "delete"],
+    },
+    {
+        "id": 2,
+        "username": "zoo",
+        "email": "zoo@zoo.com",
+        "role": "user",
+        "permissions": ["read", "write", "delete"],
+    },
+]
 
 # Псевдо-хранилище токенов (в реальности -> Redis/БД)
 issued_tokens = {"access": {}, "refresh": {}}
@@ -53,7 +63,7 @@ def create_token(token_type: str, user_id: int, expire_delta: timedelta):
     """Создание мокового токена"""
     token = secrets.token_hex(16)
     expires = datetime.utcnow() + expire_delta
-    issued_tokens[token_type][token] = {"user_id": user_id, "expires": expires}
+    issued_tokens[token_type][token] = {"sub": user_id, "expires": expires}
     return token
 
 
@@ -67,64 +77,68 @@ def verify_token(token_type: str, token: str):
     return data["user_id"]
 
 
-@app.post("/login")
-def login(response: Response):
-    # Мок: сразу "авторизуем"
-    user_id = mock_user["id"]
+# @app.post("/login")
+# def login(response: Response):
+#     # Мок: сразу "авторизуем"
+#     user_id = mock_users["id"]
 
-    access_token = create_token(
-        "access", user_id, timedelta(minutes=ACCESS_EXPIRE_MINUTES)
-    )
-    refresh_token = create_token(
-        "refresh", user_id, timedelta(days=REFRESH_EXPIRE_DAYS)
-    )
+#     access_token = create_token(
+#         "access", user_id, timedelta(minutes=ACCESS_EXPIRE_MINUTES)
+#     )
+#     refresh_token = create_token(
+#         "refresh", user_id, timedelta(days=REFRESH_EXPIRE_DAYS)
+#     )
 
-    # Устанавливаем токены в HttpOnly cookie
-    response.set_cookie(
-        key="access_token",
-        value=access_token,
-        httponly=True,
-        max_age=ACCESS_EXPIRE_MINUTES * 60,
-        secure=False,
-        samesite="lax",
-    )
-    response.set_cookie(
-        key="refresh_token",
-        value=refresh_token,
-        httponly=True,
-        max_age=REFRESH_EXPIRE_DAYS * 24 * 60 * 60,
-        secure=False,
-        samesite="lax",
-    )
+#     # Устанавливаем токены в HttpOnly cookie
+#     response.set_cookie(
+#         key="access_token",
+#         value=access_token,
+#         httponly=True,
+#         max_age=ACCESS_EXPIRE_MINUTES * 60,
+#         secure=False,
+#         samesite="lax",
+#     )
+#     response.set_cookie(
+#         key="refresh_token",
+#         value=refresh_token,
+#         httponly=True,
+#         max_age=REFRESH_EXPIRE_DAYS * 24 * 60 * 60,
+#         secure=False,
+#         samesite="lax",
+#     )
 
-    return {"message": "Logged in successfully"}
+#     return {"message": "Logged in successfully"}
 
 
 @app.get("/me")
-def get_me(access_token: str | None = Cookie(default=None)):
-    """Защищённый эндпоинт"""
-    if not access_token or not verify_token("access", access_token):
-        raise HTTPException(status_code=401, detail="Not authorized")
-    return mock_user
+def get_me(id: int):
+    # ищем пользователя
+    user = next((u for u in mock_users if u["id"] == id), None)
+    if not user:
+        return JSONResponse(content={"detail": "User not found"}, status_code=404)
+
+    response = JSONResponse(content=user)
+    response.headers["My_custom_wwwwww8"] = "suka"  # кастомный заголовок
+    return response
 
 
-@app.post("/refresh")
-def refresh_token(response: Response, refresh_token: str | None = Cookie(default=None)):
-    if not refresh_token or not verify_token("refresh", refresh_token):
-        raise HTTPException(status_code=401, detail="Invalid refresh token")
+# @app.post("/refresh")
+# def refresh_token(response: Response, refresh_token: str | None = Cookie(default=None)):
+#     if not refresh_token or not verify_token("refresh", refresh_token):
+#         raise HTTPException(status_code=401, detail="Invalid refresh token")
 
-    user_id = issued_tokens["refresh"][refresh_token]["user_id"]
+#     user_id = issued_tokens["refresh"][refresh_token]["user_id"]
 
-    new_access_token = create_token(
-        "access", user_id, timedelta(minutes=ACCESS_EXPIRE_MINUTES)
-    )
-    response.set_cookie(
-        key="access_token",
-        value=new_access_token,
-        httponly=True,
-        max_age=ACCESS_EXPIRE_MINUTES * 60,
-        secure=False,
-        samesite="lax",
-    )
+#     new_access_token = create_token(
+#         "access", user_id, timedelta(minutes=ACCESS_EXPIRE_MINUTES)
+#     )
+#     response.set_cookie(
+#         key="access_token",
+#         value=new_access_token,
+#         httponly=True,
+#         max_age=ACCESS_EXPIRE_MINUTES * 60,
+#         secure=False,
+#         samesite="lax",
+#     )
 
-    return {"message": "Access token refreshed"}
+#     return {"message": "Access token refreshed"}
